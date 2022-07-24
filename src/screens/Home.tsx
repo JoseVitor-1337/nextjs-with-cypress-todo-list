aa2df3ab0ced6165f778f6105882f393d30ea1a0
JoseVitor-1337 <josevitordemelo@yahoo.com>
2022-07-24 04:24:53 -0300

    Feature: instalando a dependência prettier.

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -3,7 +3,7 @@ import Navbar from 'components/Navbar'
 export default function Home() {
   return (
     <main>
-      <Navbar />
+      <Navbar />                
       <h1>Home</h1>
     </main>
   )
